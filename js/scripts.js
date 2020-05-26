142edb9d2a55cb90f384371f1131a52c6d481f5a
pomehy <hlopkov.gleb@mail.ru>
2020-05-26 21:03:44 +0300

    fix

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$('.main-nav__link, .logo[href^="#"], *[data-href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});
+});
+
+$(".price__item").not(":first").hide();
+$(".offer .offer__item").click(function() {
+	$(".offer .offer__item").removeClass("active").eq($(this).index()).addClass("active");
+	$(".price__item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
